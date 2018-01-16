1cd32db1964a5631a143dd2f6c42932a450f0573
Jakub Kolar <jk.oolar@gmail.com>
2018-01-16 20:33:17 +0100

    Add filtering and sorting

--- archive-eshop.php
+++ archive-eshop.php
@@ -14,16 +14,30 @@
  * @since   Timber 0.2
  */
 
+global $wp_query;
+
 $context = Timber::get_context();
 
 $context['posts'] = new Timber\PostQuery();
+$context['found_posts'] = $wp_query->found_posts;
+$context['archive_link'] =  get_post_type_archive_link( get_post_type() );
 $context['all_categories'] = Timber::get_terms('eshop_category');
 $context['type_choices'] = [
-	'Koupit e-shop',
-	'Půjčit e-shopu',
-	'Investovat do e-shopu',
+	0 => 'Koupit e-shop',
+	1 => 'Půjčit e-shopu',
+	2 => 'Investovat do e-shopu',
+];
+$context['order_choices'] = [
+	'date_desc' => 'Nejnověji přidáno',
+	'price_asc' => 'Nejlevnější',
+	'price_desc' => 'Nejdražší',
+	'turnover_asc' => 'Nejmenší obrat',
+	'turnover_desc' => 'Největší obrat',
+	'age_desc' => 'Nejstarší',
+	'age_asc' => 'Nejmladší',
 ];
 $context['checked_categories'] = isset($_GET[ 'category' ]) ? $_GET[ 'category' ] : [];
 $context['checked_types'] = isset($_GET[ 'type' ]) ? $_GET[ 'type' ] : [];
+$context['selected_orderby'] = isset($_GET[ 'orderby' ]) ? $_GET[ 'orderby' ] : null;
 
 Timber::render( 'archive-eshop.twig', $context );
--- lib/site.php
+++ lib/site.php
@@ -34,11 +34,9 @@ class EshopisteSite extends TimberSite {
 	}
 
 	function add_to_context( $context ) {
-		$context['foo'] = 'bar';
-		$context['stuff'] = 'I am a value set in your functions.php file';
-		$context['notes'] = 'These values are available everytime you call Timber::get_context();';
 		$context['menu'] = new TimberMenu();
 		$context['site'] = $this;
+		$context['current_url'] = Timber\URLHelper::get_current_url();
 		return $context;
 	}
 
